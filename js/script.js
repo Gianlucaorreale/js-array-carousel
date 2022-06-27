bc7d3cedd33021d85e879673c1f1fced56dac5ad
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-06-27 17:45:33 +0200

    fixed-milestone-2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const images = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg","img/06.jpg","img/07.jpg","img/08.jpg","img/09.jpg","img/10.jpg", ];
+const imageList = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg","img/06.jpg","img/07.jpg","img/08.jpg","img/09.jpg","img/10.jpg", ];
 let courrentActiveIndex = 0;
-
+const gallery = document.getElementById('gallery');
 
 
 let elements = '';
 
-for(let i=0; i<images.length; i++){
-    elements += `${images[i]}`;
+for(let i=0; i<imageList.length; i++){
+    elements += `<img class="photo" src="${imageList[i]}">`;
 }
 
 gallery.innerHTML= elements;
+const images = document.getElementsByClassName('photo');
+
 
 images [courrentActiveIndex].classList.add('active')
 
@@ -30,7 +32,7 @@ nextButton.addEventListener('click' , function(){
 
 prevButton.addEventListener('click' , function(){
     images[courrentActiveIndex].classList.remove('active');
-    courrentActiveIndex++;
+    courrentActiveIndex--;
 
     if (courrentActiveIndex === images.length){
         courrentActiveIndex = 0
